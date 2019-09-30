fa3dbc0f14068cf9943d3c52947ecb544d76b5e3
Hannah <hannah.alcantara14@gmail.com>
2019-09-29 18:41:49 -0700

    Added Contact Us

--- js/script.js
+++ js/script.js
@@ -3,55 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems);
 });
 
-// // /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-// // function openNav() {
-// //     document.getElementById("mySidebar").style.width = "250px";
-// //     document.getElementById("main").style.marginLeft = "250px";
-// //   }
-  
-// //   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-// //   function closeNav() {
-// //     document.getElementById("mySidebar").style.width = "0";
-// //     document.getElementById("main").style.marginLeft = "0";
-// //   }
+$('.contact-link').click(function (e) {
+    $('html,body').animate({
+        scrollTop: ($('#contact').offset().top - navHeight)
+    }, 300);
+});
 
-// //   $(document).ready(function () {
-// //     $('.sidenav').sidenav();
-// //     });
-
-// $(document).ready(function () {
-//   $('.parallax').parallax();
-//   var stickyNavTop = $('body').offset().top;
-//   var navHeight = $('.navbar').height();
-//   $('.home-link').click(function (e) {
-//       // e.preventDefault();
-//       $('html,body').animate({
-//           scrollTop: $('#home').offset().top
-//       }, 300);
-//   });
-//   $('.about-link').click(function (e) {
-//       // e.preventDefault();
-//       $('html,body').animate({
-//           scrollTop: ($('#about').offset().top - navHeight)
-//       }, 300);
-//   });
-//   // $('.project-link').click(function (e) {
-//   //     e.preventDefault(); 
-//   //     $('html,body').animate({
-//   //         scrollTop: ($('#project').offset().top - navHeight)
-//   //     }, 300);
-//   // });
-//   $('.news-link').click(function (e) {
-//       // e.preventDefault();
-//       $('html,body').animate({
-//           scrollTop: ($('#news').offset().top - navHeight)
-//       }, 300);
-//   });
-//   $('.contact-link').click(function (e) {
-//       $('html,body').animate({
-//           scrollTop: ($('#contact').offset().top - navHeight)
-//       }, 300);
-//   });
-
-
-  
+function onSubmit(e) {
+    e.preventDefault();
+    M.toast({html: 'Form Submitted!'})
+}
